c69d7a4abbb46713ee5ec02371fcd48c46c0faa2
vladislav-chihun <vladikkosmo@gmail.com>
2024-01-24 12:54:10 +0100

    f

--- js/task-4.js
+++ js/task-4.js
@@ -1,14 +1,24 @@
 function getShippingCost(country) {
-    switch (country) {
-        case "Australia": console.log("Shipping to Australia will cost 170 credits");
-        case "Germany": console.log("Sorry, there is no delivery to your country");
-        case "China": console.log("Shipping to China will cost 100 credits");
-        case "Chile": console.log("Shipping to Chile will cost 250 credits");
-        case "Jamaica": console.log("Shipping to Jamaica will cost 120 credits");
-        case "Sweden": console.log("Sorry, there is no delivery to your country");
-    }
     
+    switch (country) {
+        case "Australia":
+            return "Shipping to Australia will cost 170 credits";
+            
+        case "China": 
+            return "Shipping to China will cost 100 credits";
+
+        case "Chile": 
+            return "Shipping to Chile will cost 250 credits";
+            
+        case "Jamaica": 
+            return "Shipping to Jamaica will cost 120 credits";
+            
+        default:
+            return "Sorry, there is no delivery to your country";
+    }
+
 }
+
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
 console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
